deb2cf55819a7a8edc88a3c41e11444e0aaca230
shubham30102002 <shubhamrg30@gmail.com>
2024-02-05 12:41:35 +0530

    question 5

--- question5/src/App.js
+++ question5/src/App.js
@@ -1,4 +1,21 @@
 import FocusableInput from "./components/FocusableInput";
+import React, {useRef, useEffect} from 'react'
+
+const Input = React.forwardRef((props,ref =>{ 
+    return <input {...props} ref={ref}/>
+
+}))
+
+const FocusableInput = React.forwardRef((props, ref) =>{
+    useEffect(() =>{
+        const inputRef = ref.current;
+        if(props.focused && inputRef){
+            inputRef.focus();
+        } 
+    },[props.focused()])
+
+    return <input ref={ref} />
+})
 
 const App = (props) => <FocusableInput focused={props.focused} />;
 
